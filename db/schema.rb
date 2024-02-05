de1269310b5180770dd87e4cd13113f84ebe45a5
Joshua Yee <joshyee16@gmail.com>
2024-02-05 13:15:54 -0800

    add Follows model and updated follows and user model method associations (no follows routes/controller)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_05_200536) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_05_210731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_05_200536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "director_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "genres", force: :cascade do |t|
